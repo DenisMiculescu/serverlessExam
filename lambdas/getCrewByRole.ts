75cf24c2de5dd5885585376caf5c4640419ffd9b
denis miculescu <20098078@mail.wit.ie>
2024-12-09 13:01:34 +0000

    Amended new endpoint to include query string

--- lambdas/getCrewByRole.ts
+++ lambdas/getCrewByRole.ts
@@ -5,6 +5,7 @@ const { CREW_TABLE_NAME } = process.env;
 
 export const handler = async (event: any) => {
   const { role, movieId } = event.pathParameters;
+  const { name } = event.queryStringParameters || {};
 
   if (!role || !movieId) {
     return {
@@ -25,9 +26,29 @@ export const handler = async (event: any) => {
       })
       .promise();
 
+    // New Functionality to filter by name for Q2 B
+    let filteredItems = result.Items || [];
+    if (name) {
+      const lowerCaseName = name.toLowerCase();
+      filteredItems = filteredItems.flatMap((item) => {
+
+        const nameArray = item.names.split(",").map((n: string) => n.trim());
+
+        const matchingNames = nameArray.filter((n) =>
+          n.toLowerCase().includes(lowerCaseName)
+        );
+
+        return matchingNames.map((matchingName) => ({
+          movieId: item.movieId,
+          crewRole: item.crewRole,
+          name: matchingName,
+        }));
+      });
+    }
+
     return {
       statusCode: 200,
-      body: JSON.stringify(result.Items || []),
+      body: JSON.stringify(filteredItems),
     };
   } catch (error) {
     console.error("Error querying crew members:", error);
